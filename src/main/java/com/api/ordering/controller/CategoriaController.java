4ee0a415bb6f2b219e9dceed729abcb6620b788f
Santos <alanjosedossantos011@gmail.com>
2021-01-24 01:56:51 -0300

    Testando a primeira classe de dominio

--- src/main/java/com/api/ordering/controller/CategoriaController.java
+++ src/main/java/com/api/ordering/controller/CategoriaController.java
@@ -1,15 +1,25 @@
 package com.api.ordering.controller;
 
+import com.api.ordering.model.Categoria;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("categorias")
 public class CategoriaController {
 
     @GetMapping
-    public String listar(){
-        return "Resta está funcionando.";
+    public List<Categoria> listar(){
+        Categoria categoria1 = new Categoria(1, "Informatica");
+        Categoria categoria2 = new Categoria(2, "Mouse");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(categoria1);
+        lista.add(categoria2);
+        return lista;
     }
 }
