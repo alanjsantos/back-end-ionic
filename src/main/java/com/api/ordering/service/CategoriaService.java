7599b595ac683c867d3934cc346f59afe513ef69
Santos <alanjosedossantos011@gmail.com>
2021-01-25 11:47:57 -0300

    Ajuste no endpoint /categoria /{id}

--- src/main/java/com/api/ordering/service/CategoriaService.java
+++ src/main/java/com/api/ordering/service/CategoriaService.java
@@ -2,6 +2,7 @@ package com.api.ordering.service;
 
 import com.api.ordering.model.Categoria;
 import com.api.ordering.repository.CategoriaRepository;
+import com.api.ordering.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +17,7 @@ public class CategoriaService {
     public Categoria buscarPorId(Integer id){
         Optional<Categoria> categoria = repository.findById(id);
 
-        return categoria.orElse(null);
+        return categoria.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
     }
 }
