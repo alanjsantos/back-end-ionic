18998ddb990179870f24a7c75881c5ae49c66f39
Santos <alanjosedossantos011@gmail.com>
2021-01-26 16:54:00 -0300

    Pedido, pagamento e Estado Pagamento

---
+++ src/main/java/com/api/ordering/repository/PagamentoRepository.java
@@ -0,0 +1,9 @@
+package com.api.ordering.repository;
+
+import com.api.ordering.model.Pagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
+}
