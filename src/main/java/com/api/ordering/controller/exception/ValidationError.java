c65a2fe34c633bdf5189bcff3f155f525c39edcf
Santos <alanjosedossantos011@gmail.com>
2021-01-29 22:09:39 -0300

    Personalizando o retorno das validações Categoria

---
+++ src/main/java/com/api/ordering/controller/exception/ValidationError.java
@@ -0,0 +1,23 @@
+package com.api.ordering.controller.exception;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.FileHandler;
+
+public class ValidationError extends StandardError{
+    private static final long serialVersionUID = 1L;
+
+    private List<FieldMessage> errors = new ArrayList<>();
+
+    public ValidationError(Integer status, String msg, Long timeStamp) {
+        super(status, msg, timeStamp);
+    }
+
+    public List<FieldMessage> getErrors() {
+        return errors;
+    }
+
+    public void addError(String fieldName, String mensagem){
+        errors.add(new FieldMessage(fieldName, mensagem));
+    }
+}
